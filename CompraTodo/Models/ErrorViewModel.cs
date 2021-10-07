7a37c4673b6639253082bc321ad3b159e8506b34
Sergionx <sergio180203@gmail.com>
2021-10-07 19:46:20 -0400

    Add project files.

---
+++ CompraTodo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompraTodo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
