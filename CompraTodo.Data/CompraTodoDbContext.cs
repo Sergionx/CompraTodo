bf73601dcf112a450fa57109ffe534e17fcf8b47
Sergionx <sergio180203@gmail.com>
2021-10-08 20:51:34 -0400

    Template from Visual Studio

---
+++ CompraTodo.Data/CompraTodoDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompraTodo.Data
+{
+    public class CompraTodoDbContext: DbContext
+    {
+        public CompraTodoDbContext(DbContextOptions<CompraTodoDbContext> options)
+            :base(options)
+        {
+
+        }
+
+    }
+}
